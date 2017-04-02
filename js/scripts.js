30fd20d1623fc8d840cfe085d22380cbf8a47676
rouz majlessi <rmajlessi@gmail.com>
2017-04-01 17:18:37 -0700

    call function we defined earler

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-
+// defined fucntion but cannot do anyting because we have not *Called* the functions as of yet.
 var speak = function(saySomething) {
   alert(saySomething);
 };
@@ -6,3 +6,5 @@ var speak = function(saySomething) {
 var subtract = function(number1,number2) {
   return number1 - number1;
 };
+// We know * Call* the function speak which we defined abve
+speak("this is calling the function");
